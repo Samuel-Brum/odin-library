d430ee2ff546b2fa6d20137cd3fb9c480b1d4e66
Samuel-Brum <samuelbrummartins@gmail.com>
2023-01-31 18:47:58 -0300

    Implementa inserção de livros na DOM

--- script.js
+++ script.js
@@ -12,24 +12,31 @@ function addBookToLibrary(newBook, library) {
   library.push(newBook);
 }
 
+function addCard(string, elementName) {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = `<pre>${string}</pre>`;
+  document.getElementById(elementName).appendChild(card);
+}
+
 function displayBooks(library) {
   for (book in library) {
     if (Object.prototype.hasOwnProperty.call(library, book)) {
-      const string = 'Livro: ' + library[book].title + '\n' +
-      'Autor: ' + library[book].author + '\n' +
-      'Ano de publicação: ' + library[book].year;
+      const bookInfo = 'Book: ' + library[book].title + '\n' +
+      'Author: ' + library[book].author + '\n' +
+      'Year of Publication: ' + library[book].year;
 
-      string.replace(/\t/g, ''); // removes indentation
-
-      console.log(string);
+      addCard(bookInfo, 'showcase');
     }
   }
 }
 
 const lotr = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1921);
 const babel = new Book('Babel', 'R.F. Kuang', 2022);
+const psalmFTW = new Book('A Psalm for the Wild', 'Becky Chambers', 2021);
 
 addBookToLibrary(lotr, myLibrary);
 addBookToLibrary(babel, myLibrary);
+addBookToLibrary(psalmFTW, myLibrary);
 
 displayBooks(myLibrary);
